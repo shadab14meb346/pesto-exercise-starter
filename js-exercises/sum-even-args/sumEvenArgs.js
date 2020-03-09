7c6e50f63fa3c0d1441acf808997f674e36d78b9
Shadab <shadab14meb346@gmail.com>
2020-03-10 00:50:49 +0530

    enhance sum-even-args solution
    oneliner isSum function

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,8 +1,6 @@
+const isSum = number => number % 2 === 0;
 const sumEvenArgs = (...args) => {
-  let isSum = number => {
-    if (number === 1) return false;
-    return !Boolean(number % 2);
-  }
+
   let onlySumArguments = args.filter(isSum);
   return onlySumArguments.reduce((sumEvenArgs, currentValue) => {
     return sumEvenArgs + currentValue;
