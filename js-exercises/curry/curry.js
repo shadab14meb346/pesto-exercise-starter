991d6269c1677591470624ad2e41d40d8544bd59
Shadab <shadab14meb346@gmail.com>
2020-03-10 21:30:32 +0530

    update namings

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,7 +1,7 @@
-function curry(functionArgument) {
+function curry(callback) {
   return function curried(...args) {
-    if (args.length >= functionArgument.length) {
-      return functionArgument.apply(this, args);
+    if (args.length >= callback.length) {
+      return callback.apply(this, args);
     } else {
       return function (...args2) {
         return curried.apply(this, args.concat(args2));
