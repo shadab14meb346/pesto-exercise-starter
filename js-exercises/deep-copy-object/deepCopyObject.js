4d4c445ff039829e8ffce79d641fd0b350d999fd
Shadab <shadab14meb346@gmail.com>
2020-03-10 20:35:15 +0530

    solve deep-copy-object problem

--- js-exercises/deep-copy-object/deepCopyObject.js
+++ js-exercises/deep-copy-object/deepCopyObject.js
@@ -1,22 +1,13 @@
 const deepCopyObject = objToCopy => {
-  let objectCopy = {}
-  for (const key in objToCopy) {
-    if (typeof objToCopy[key] === "object") {
-      for (const subKey in objToCopy[key]) {
-        let subObjectCopy = {};
-        if (typeof objToCopy[key][subKey] === "object") {
-          return deepCopyObject(objToCopy[key][subKey]);
-        } else {
-          subObjectCopy[subKey] = objToCopy[key][subKey];
-        }
-      }
-    } else {
-      objectCopy[key] = objToCopy[key];
-    }
+  if (typeof objToCopy !== "object" || objToCopy === null) {
+    return objToCopy;
   }
-  return objectCopy;
+  const copyObject = {};
+  for (const key in objToCopy) {
+    copyObject[key] = deepCopyObject(objToCopy[key]);
+  }
+  return copyObject;
 };
-
 export {
   deepCopyObject
 };
